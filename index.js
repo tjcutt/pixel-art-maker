e87ee9db7da52010e2341a9645c8119cd8a8127c
Tim <tjcutt@gmail.com>
2017-02-11 15:53:00 -0700

    colors but pixels are not working

--- index.js
+++ index.js
@@ -1,62 +1,115 @@
+// // CREATING ELEMENTS
+// //===================================
 
-// CREATING ELEMENTS
-//===================================
-
-
+let header = document.createElement('header')
 let pixelSection = document.createElement('section');
 let body = document.getElementsByTagName('body')[0];
-let paletteAside = document.createElement('aside');
-
-let palette = document.createElement('div');
+let paletteSection = document.createElement('paletteSection');
+// let palette = document.createElement('div');
+// let header  = document.createElement('header');
 
 
 
 // APPENDING
 //===================================
-body.append(pixelSection)
-body.append(paletteAside)
-paletteAside.append(palette)
+// body.appendChild(header)
+body.appendChild(pixelSection)
+body.appendChild(paletteSection)
+// paletteSection.appendChild(palette)
+
 
 
 // SETTING CLASSES
 //===================================
 
 pixelSection.classList.add('pixelSection');
-paletteAside.classList.add('paletteAside');
+paletteSection.classList.add('paletteSection');
 
 // CREATING DIVS
 //===================================
-for (var i = 0; i < 500; i++) {
-  let div = document.createElement('div');
-  pixelSection.append(div);
-  div.classList.add('pixels')
-  // pixels.style.boxSizing = 'borderBox';
-  div.style.width = '0.8em';
-  div.style.height = '0.8em';
-  div.style.display = 'block'
-  div.style.border = '1px solid black';
-  div.style.float = 'left'
-  div.style.borderCollapse = 'collapse';
+
+for (var i = 0; i < 2000; i++) {
+    let div = document.createElement('div')
+    pixelSection.append(div);
+    div.className = 'pixel'
 }
 
-//            STYLES
-//===================================
-//pixel section styling
-//===================================
-pixelSection.style.width  = '80%';
-pixelSection.style.height = '400px'
-pixelSection.style.display = 'block';
-pixelSection.style.float = 'left';
+// created a div element, appended it to the pixelSection, set a class name to pixel
+
+//find a way to sort by colors
+
+for (var i = 0; i < 50; i++) {
+
+    let colors = document.createElement('div');
+    paletteSection.appendChild(colors);
+    colors.className = 'colors';
+    colors.id='colors'+i
+    let randomColor = 'rgb(' + (Math.floor(Math.random()*256)) + ', ' +  (Math.floor(Math.random()*256)) + ', ' + (Math.floor(Math.random()*256)) + ')';
+    colors.style.backgroundColor = randomColor
+}
 
 
+// CREATING COLORS
 //===================================
-//palette aside styling
-//===================================
-paletteAside.style.width = '200px'
+var colorPlaceholder = ''
+
+paletteSection.addEventListener('click', function(event){
+  // console.log('Event target', event.target);
+  colorPlaceholder =  event.target.style.backgroundColor
+console.log(style);
+});
+
+pixelSection.addEventListener('click', function(event){
+  pixelSection.setAttribute('style.backgrou;
+})
 
 
-//===================================
-//palette div styling
-//===================================
-palette.style.borderRadius = '20px'
-// palette.style.border = '1px solid black'
+
+
+
+
+
+
+
+
+// for (var i = 0; i < 11; i++) {
+//   let div = document.createElement('div')
+//   div.id = 'myroylgavpbw'[i]
+//   palette.appendChild(div)
+// }
+
+
+
+//   let div = document.createElement('div');
+//   pixelSection.append(div);
+//   div.classList.add('pixels')
+//   // pixels.style.boxSizing = 'borderBox';
+//   div.style.width = '0.8em';
+//   div.style.height = '0.8em';
+//   div.style.padding = 'auto'
+//   div.style.display = 'block'
+//   div.style.border = '1px solid black';
+//   div.style.float = 'left'
+//   div.style.borderCollapse = 'collapse';
+// }
+//
+// //            STYLES
+// //===================================
+// //pixel section styling
+// //===================================
+// pixelSection.style.width  = '80%';
+// pixelSection.style.display = 'block';
+// pixelSection.style.float = 'left';
+//
+//
+// //===================================
+// //palette aside styling
+// //===================================
+// paletteSection.style.width = '200px'
+//
+//
+// //===================================
+// //palette div styling
+// //===================================
+// palette.style.borderRadius = '20px'
+// // palette.style.border = '1px solid black'
